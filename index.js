2d342e9a1c6ff00d11a270bca21b4d1cc794df70
jade decordova <lacroixjaded@gmail.com>
2020-03-27 14:55:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 function dwarfRollCall(dwarves) {
+ let dwarf = []
+// for(let dwarve = 0; dwarve< dwarves.length; dwarve++ ){
+  // let dwarfCount = parseInt(dwarve) + 1;
+   //let theDwarf = String(dwarfCount) +'. '+ `${dwarves[dwarve]}`
+   //dwarf.push(theDwarf );
+ //}
+// return String(dwarf);
 }
+
+function dwarfRollCall(dwarves) {
+   var newStr = "";
+ dwarves.forEach(function(name, Pos) {
+   	newStr += (Pos+1 + ". " + name + " ");
+   });
+  return newStr;
+}
+
+
 
 function summonCaptainPlanet(planeteerCalls){
+  let array = [];
+  for (let i=0;i<planeteerCalls.length; i++){
+    array.push(`${planeteerCalls[i]}!`.toUpperCase())
+    
+  }
+  return array
 }
 
-function longPlaneteerCalls(words) {
+function longPlaneteerCalls(words){
+  for(var i=0;i<words.length;i++){
+    if (words[i].length>4){
+      return true
+    } else {
+    return false  
+    }
+  }
 }
 
-function findTheCheese (foods) {
+function findTheCheese (food) {
+  for(let i=0; i < food.length ; i++) {
+    if (food[i] === 'camembert' || food[i] === 'gouda' || food[i] === 'cheddar') {
+      return food[i]
+    }
+  }
+  return "no cheese!"
 }
